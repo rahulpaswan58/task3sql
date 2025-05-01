6ca53498ae83f6b79a44a7c81baacf412ee5582b
rahulpaswan58 <paswanrahul4811@gmail.com>
2025-05-01 14:29:05 +0530

    Add files via upload

---
+++ orders.sql
@@ -0,0 +1,19 @@
+SELECT * FROM orders
+
+SELECT * FROM orders WHERE waiter = '5';
+
+SELECT * FROM orders WHERE cusine = 'Thai';
+
+SELECT * FROM orders WHERE dish = 'Pizza';
+
+SELECT * FROM orders WHERE restaurantid = '7';
+
+SELECT * FROM orders WHERE id = '32';
+
+SELECT * FROM orders WHERE order_date = '2025-04-2';
+
+SELECT * FROM orders WHERE total_amount BETWEEN 300 AND 400;
+
+SELECT * FROM orders WHERE payment_method = 'UPI';
+
+SELECT * FROM orders WHERE order_status = 'preparing';
